b09c630fd52b5ef1ddf63a2f7d07294d57ccd41c
Venkata Bhattaram <tinitiate@users.noreply.github.com>
2019-06-14 15:08:53 -0400

    1

---
+++ code/002-numpy-create-arrays-advanced.py
@@ -0,0 +1,43 @@
+""" MARKDOWN
+---
+YamlDesc: CONTENT-ARTICLE
+Title: Python NumPy
+MetaDescription: Python NumPy
+MetaKeywords: Python Pandas create using np.arange, np.linspace, np.logspace
+Author: (c) Venkata Bhattaram / tinitiate.com
+ContentName: python-numpy-advanced-array-creation
+---
+MARKDOWN """
+
+""" MARKDOWN
+# Python NumPy
+* NumPy is a general-purpose array-processing Python package, for scientific 
+  data computing.
+* The array is a multidimensional object with high-performance and provides 
+  power tools out of the box for array data processing.
+
+## NumPy Array
+* Here we demonstrate various advanced ways for array creation
+* `arange` creates numbers between 0,30 with distance of 5
+* `linspace` Create 10 even spaced numbers between 0 and 5
+* `logspace` Create 10 even spaced numbers on log scale between 0 and 5
+MARKDOWN """
+
+# MARKDOWN ```
+
+import numpy as np
+
+# arange create numbers between 0,30 with distance of 5
+a1 = np.arange(0, 36, 6) 
+print(a1)
+
+# Create 10 even spaced numbers between 0 and 5
+a2 = np.linspace(0, 5, 10) 
+print(a2)
+
+# Create 10 even spaced numbers on a log scale between 0 and 5
+a2 = np.logspace(0, 5, 10) 
+print(a2)
+
+
+# MARKDOWN ```
---
+++ code/005-numpy-dataanalysis-functions.py
@@ -0,0 +1,74 @@
+""" MARKDOWN
+---
+YamlDesc: CONTENT-ARTICLE
+Title: Python NumPy Trigonometric Functions for Data Analysis
+MetaDescription: Python NumPy Trigonometric Functions for Data Analysis
+MetaKeywords: Python, NumPy, Trigonometric Functions, Data Analysis Functions
+Author: (c) Venkata Bhattaram / tinitiate.com
+ContentName: numpy-data-analysis-trigonometric-functions
+---
+MARKDOWN """
+
+
+""" MARKDOWN
+# NumPy Trigonometric Functions
+* In Right Angle Triangles
+* sine:    opposite/hypotenuse
+  * Trigonometric sine, element-wise.
+  * This is implemented by numpy.sin
+* cosine:  adjacent/hypotenuse
+  * Cosine element-wise.
+  * This is implemented by numpy.cos  
+* tangent: opposite/adjacent
+  * Compute tangent element-wise.
+  * This is implemented by numpy.tan
+* Here we demonstrate the SIN, COS, TAN data and its representation on a graph.
+MARKDOWN """
+
+
+# MARKDOWN ```
+import numpy as np
+import matplotlib.pyplot as plt
+
+# SIN Data
+##########
+in_array = np.linspace(-np.pi, np.pi, 12)
+out_array = np.sin(in_array)
+print(out_array)
+
+# red for numpy.sin()
+plt.plot(in_array, out_array, color = 'red', marker = "o")
+plt.title("numpy.sin() SET-1")
+plt.xlabel("X")
+plt.ylabel("Y")
+plt.show()
+
+
+# COS Data
+##########
+in_array = np.linspace(-np.pi, np.pi, 12)
+out_array = np.cos(in_array)
+print(out_array)
+
+# red for numpy.cos()
+plt.plot(in_array, out_array, color = 'red', marker = "o")
+plt.title("numpy.cos() SET-1")
+plt.xlabel("X")
+plt.ylabel("Y")
+plt.show()
+
+
+# TAN Data
+##########
+in_array = np.linspace(-np.pi, np.pi/2, np.pi)
+out_array = np.tan(in_array)
+print(out_array)
+
+# red for numpy.tan()
+plt.plot(in_array, out_array, color = 'red', marker = "o")
+plt.title("numpy.tan() SET-1")
+plt.xlabel("X")
+plt.ylabel("Y")
+plt.show()
+
+# MARKDOWN ```
